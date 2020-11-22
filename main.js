1b18e594ca80dfdc07d585d537402bb1613fbe87
Darren Zheng <Yiruma@darrens-mbp.bc.edu>
2020-11-22 17:46:04 -0500

    commit mentalHealth data

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
 import { FirearmLaws } from './firearmLaws/firearmLaws.js';
-import { MentalHealth } from './mentalHealth/mentalHealth.js';
 
 d3.csv('./firearmLaws/firearmLaws.csv', d3.autoType).then(data => {
-    console.log(data);
+    // console.log(data);
     FirearmLaws('.firearm-laws-graph');
 
 })
 
-d3.csv('./mentalHealth/mentalHealth.csv', d3.autoType).then(data => {
-    console.log(data);
-    MentalHealth('.mental-health-graph');
-})
 
